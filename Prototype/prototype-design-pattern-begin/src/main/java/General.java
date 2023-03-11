2700b298f40bc7fc9ca7536ad86da53f54c7373e
mehdizebhi <mehdizebhi@yahoo.com>
2023-03-11 22:02:08 +0330

    Add Prototype design pattern example

---
+++ Prototype/prototype-design-pattern-begin/src/main/java/General.java
@@ -0,0 +1,15 @@
+
+//Doesn't support cloning
+public class General extends GameUnit{
+
+	private String state = "idle";
+	
+	public void boostMorale() {
+		this.state = "MoralBoost";
+	}
+	
+	@Override
+	public String toString() {
+		return "General "+state+" @ "+getPosition();
+	}
+}
