52679680befab111ce9beafeccb41c42361412c1
mehdizebhi <mehdizebhi@yahoo.com>
2023-05-31 15:27:29 +0330

    Implement the Decorator design pattern

--- Structural Design Patterns/Decorator/message-encoder-end/src/main/java/Client.java
+++ Structural Design Patterns/Decorator/message-encoder-end/src/main/java/Client.java
@@ -4,5 +4,11 @@ public class Client {
 		Message m = new TextMessage("The <FORCE> is strong with this one!");
 		System.out.println(m.getContent());
 
+		Message decorator = new HtmlEncodedMessage(m);
+		System.out.println(decorator.getContent());
+
+		decorator = new Base64EncodedMessage(decorator);
+		System.out.println(decorator.getContent());
+
 	}
 }
--- Structural Design Patterns/Decorator/message-encoder-end/src/main/java/HtmlEncodedMessage.java
+++ Structural Design Patterns/Decorator/message-encoder-end/src/main/java/HtmlEncodedMessage.java
@@ -1,4 +1,16 @@
-//Decorator. Implements component interface
-public class HtmlEncodedMessage {
+import org.apache.commons.text.StringEscapeUtils;
 
+//Decorator. Implements component interface
+public class HtmlEncodedMessage implements Message{
+
+    private Message msg;
+
+    public HtmlEncodedMessage(Message msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String getContent() {
+        return StringEscapeUtils.escapeHtml4(msg.getContent());
+    }
 }
